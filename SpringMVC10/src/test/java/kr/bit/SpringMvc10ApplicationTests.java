63b57fe99ec1f59fec462961226ee77d5e514cd9
Raphael.Shin <108261108+mingi-Shin@users.noreply.github.com>
2024-11-30 15:54:17 +0900

    Update SpringMvc10ApplicationTests.java

--- SpringMVC10/src/test/java/kr/bit/SpringMvc10ApplicationTests.java
+++ SpringMVC10/src/test/java/kr/bit/SpringMvc10ApplicationTests.java
@@ -6,8 +6,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringMvc10ApplicationTests {
 
+	@Autowired
+	private MemberRepository memberRepository;
+	
+	@Autowired
+	private PasswordEncoder encoder;
+	
 	@Test
-	void contextLoads() {
+	void createMember() {
+		Member vo = new Member();
+		vo.setUsername("winter");
+		vo.setPassword(encoder.encode("ssy4260")); //암호화
+		vo.setName("김민정");
+		vo.setRole(Role.MEMBER_READ_WRITE);
+		memberRepository.save(vo);
 	}
 
 }
